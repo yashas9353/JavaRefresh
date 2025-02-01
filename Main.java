3b1907e32e7d1b00371752aa2a481b37b216c1d9
Yashas_J <yashasjraj@gmail.com>
2025-02-01 18:25:09 +0530

    added throws exception example

--- Main.java
+++ Main.java
@@ -1,23 +1,16 @@
 public class Main {
+
+    public static synchronized void hello() throws ArithmeticException {
+        int res = 1 / 0;
+        System.out.println("Hello World!");
+    }
+
     public static void main(String[] args) {
         try {
-            Integer x = 10;
-            String y = String.valueOf(x);
-            System.out.println(y.getClass().getSimpleName());
-
-            enum Color {
-                RED, GREEN, BLUE;
-            }
-
-            System.out.println(Color.RED);
-
-            TestInterface obj = () -> {
-                System.out.println("Hello World!");
-            };
-
-            obj.hello();
-        } catch (Exception e) {
+            hello();
+        } catch (ArithmeticException e) {
             System.out.println(e);
         }
+        System.out.println("Hello World!");
     }
 }
