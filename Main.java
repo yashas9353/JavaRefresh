3045c498d617a89276134f533baa8cdaa0dcca48
Yashas_J <yashasjraj@gmail.com>
2025-02-02 18:19:50 +0530

    added extractDigit method

--- Main.java
+++ Main.java
@@ -1,42 +1,15 @@
-import java.util.*;
-
 public class Main {
+    public void extractDigit(int num) {
+        int digit = 0;
+        while (num > 0) {
+            digit = num % 10;
+            System.out.println(digit);
+            num = num / 10;
+        }
+    }
 
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-
-        // CRUD operations
-
-        list.add(10);
-        list.add(0, 20);
-        System.out.println(list.get(0));
-        list.set(0, 30);
-        list.remove(Integer.valueOf(30));
-        System.out.println(list);
-        System.out.println(list.contains(10));
-        System.out.println(list.size());
-
-        Iterator<Integer> iterator = list.iterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
-        }
-
-        HashMap<String, Integer> studentData = new HashMap<>();
-        studentData.put("yashas", 100);
-        studentData.put("Vaarshitha", 150);
-        System.out.println(studentData.get("yashas"));
-
-        list.add(0, 20);
-
-        list.sort(Comparator.reverseOrder());
-        System.out.println(list);
-
-        Collections.reverse(list);
-
-        Collections.sort(list, Comparator.reverseOrder());
-
-        System.out.println(Collections.frequency(list, 10));
-
-        System.out.println(list);
+        Main main = new Main();
+        main.extractDigit(12345);
     }
 }
